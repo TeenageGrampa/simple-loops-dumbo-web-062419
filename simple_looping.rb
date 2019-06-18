f6b84d8033945efacbfbe7ffa014c7e742dd2186
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-18 20:15:00 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,9 +4,8 @@ def loop_iterator(number_of_times)
   number_of_times = 0
   loop do 
     number_of_times += 1
+    puts "Welcome to Flatiron School's Web Development Course!"
     
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
 end
 
 def times_iterator(number_of_times)
